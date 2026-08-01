6e7624c64d16918e48e6be4212aad298a4f24a1f
jdholariya33 <jaydholariya37@gmail.com>
2026-08-01 09:40:25 +0530

    Delete 28.Practice_Que_2.py

--- 28.Practice_Que_2.py
+++
@@ -1,8 +0,0 @@
-# Practice Question 2 :
-# Que : - Write a program to input side of a square & print its area.
-
-side = float(input("Enter side length of the square : "))   
-
-area = side * side      # side ** 2
-
-print("Area of the Square : " , area)
